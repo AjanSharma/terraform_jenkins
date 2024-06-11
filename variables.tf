820b9964e8d2467ba1630a1035befcb1877410dc
AjanSharma <ajansharma7@gmail.com>
2024-06-11 00:58:39 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,43 @@
+variable "client_id" {
+ description = "Client ID"
+}
+
+variable "client_secret" {
+ description = "Client Secret"
+}
+
+variable "tenant_id" {
+ description = "Tenant ID"
+}
+
+variable "subscription_id" {
+ description = "Subscription ID"
+}
+
+variable "resource_group_location" {
+ description = "Resource Group Location"
+}
+
+variable "vm_size_val" {
+ description = "VM Size"
+}
+
+variable "publisher_val" {
+ description = "Publisher"
+}
+
+variable "offer_val" {
+ description = "Offer"
+}
+
+variable "sku_val" {
+ description = "SKU"
+}
+
+variable "admin_username_val" {
+ description = "Admin Username"
+}
+
+variable "admin_password_val" {
+ description = "Password"
+}
